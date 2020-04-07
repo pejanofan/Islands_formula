98500f09cdda555a465fef4ac96b5d026b5ef3cf
Kenneth.Chang <kenneth.chang@cyberpower.com>
2020-04-07 09:46:05 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  // 取消連結外框線
+  $("a").focus(function () {
+    $(this).blur();
+  });
+
+  $('a.smoothScroll').smoothScroll({
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+
+  $('.scrollup').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+  //指定畫面滑動
+
+  
+  $('.link-1').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-1').offset().top
+    }, 800);
+  });
+  $('.link-2').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-2').offset().top - 100
+    }, 800);
+  });
+  $('.link-3').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-3').offset().top - 100
+    }, 800);
+  });
+  $('.link-4').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-4').offset().top - 100
+    }, 800);
+  });
+  $('.link-5').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-5').offset().top - 100
+    }, 800);
+  });
+  $('.link-6').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#contentBox-6').offset().top - 100
+    }, 800);
+  });
+
+  $('.water,.water2').ripples({
+    resolution: 256,
+    dropRadius: 20,
+    perturbance: 0.04,
+  });
+});
